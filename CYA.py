aa7ae0edd5c6277e4cd582641a484de512c105b5
Cecilasah <thygloriousone@gmail.com>
2026-05-31 20:33:52 +0000

    Add files via upload

---
+++ CYA.py
@@ -0,0 +1,35 @@
+# File: initials.py
+# Description: Print out my initials in large letters.
+# Assignment Number: 1
+#
+# Name: Cecil Yaw Asah
+# STUDENT ID: 2425401803
+# Email: cecilasah32@gmail.com
+# Grader: Bukman Augutus
+#
+# On my honor, Cecil Yaw Asah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # Prints my initials in large letters
+
+    print()
+    print("...CYA")
+    print()
+
+    print("...CCCCCCCCCCCC...YY........YY...AAAAAAAAAAAA...****")
+    print("...CC........CC....YY......YY....AA........AA...****")
+    print("...CC...............YY....YY.....AA........AA.......")
+    print("...CC................YYYYYY......AAAAAAAAAAAA.......")
+    print("...CC.................YYYY.......AA........AA.......")
+    print("...CC.................YYYY.......AA........AA.......")
+    print("...CC.................YYYY.......AA........AA.......")
+    print("...CC........CC.......YYYY.......AA........AA.......")
+    print("...CCCCCCCCCCCC.......YYYY.......AA........AA.......")
+    print("................................................****")
+
+    print()
+
+
+main()
